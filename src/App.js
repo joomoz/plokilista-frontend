5807cc46380482807ec25ae7fcab063b3471e4e4
J. Moilanen <jnsmoilanen@gmail.com>
2018-02-18 19:32:05 +0200

    5.1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      blogs: []
+      blogs: [],
+      username: '',
+      password: '',
+      user: null
     }
   }
 
@@ -16,13 +21,69 @@ class App extends React.Component {
     )
   } 
 
+  login = async (event) => {
+    event.preventDefault()
+    try{
+      const user = await loginService.login({
+        username: this.state.username,
+        password: this.state.password
+      })
+  
+      this.setState({ username: '', password: '', user})
+    } catch(exception) {
+      this.setState({
+        error: 'username or password incorrect',
+      })
+      setTimeout(() => {
+        this.setState({ error: null })
+      }, 5000)
+    }
+  }
+
+  handleLoginFieldChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
   render() {
+    const loginForm = () => (
+      <div>
+        <h2>Login to plokilist app</h2>
+        <form onSubmit={this.login}>
+          <div>
+            username
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleLoginFieldChange}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleLoginFieldChange}
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    )
+
     return (
       <div>
-        <h2>blogs</h2>
-        {this.state.blogs.map(blog => 
-          <Blog key={blog._id} blog={blog}/>
-        )}
+        <h1>Plokilist frontend</h1>
+        {this.state.user === null ?
+          loginForm() :
+          <div>
+            <p>{this.state.user.name} logged in</p>
+            {this.state.blogs.map(blog => 
+              <Blog key={blog._id} blog={blog}/>
+            )}
+          </div>       
+        }
       </div>
     );
   }
